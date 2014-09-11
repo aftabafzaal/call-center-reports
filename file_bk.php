21b9b31267f78fbccd392aa21310612ebcd6fd32
aftabafzaal <aftabafzaal@live.com>
2014-09-11 15:22:03 +0500

    Update file_bk.php

--- file_bk.php
+++ file_bk.php
@@ -1,28 +1,5 @@
 <?php
-function d($array,$die=false){
-    echo "<pre>";
-    print_r($array);
-    echo "</pre>";        
-    if($die){
-     exit;   
-            
-    }    
-    
-}
-
-ini_set('memory_limit', '-1');
-ini_set('max_execution_time', 300);
-$link=mysql_connect("localhost", "root", "");
-if (!$link) 
-{
-    die('MySQL connect ERROR: ' . mysql_error());
-}
-mysql_select_db("asterisk");
-
-
-
-
-
+include("connection.php");
 
 $campaingns=array('equinox_us','equinox_uk');
 
